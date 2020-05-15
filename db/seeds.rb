fc6f70ec85c5c7f89bbb653a08ba380dfa8ac037
Bradley Petty-Fitzmaurice <bradpfm@gmail.com>
2020-05-15 18:14:35 +0100

    cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: 'Bourbon')
+Ingredient.create(name: 'Whiskey')
+Ingredient.create(name: 'Vodka')
+Ingredient.create(name: 'Bitters')
+Ingredient.create(name: 'Sugar')
+Ingredient.create(name: 'Vermouth')
